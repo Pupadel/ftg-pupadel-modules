9ef291fe9f8e1c26725f6c8994c01f6d05bc55dc
Pupadel <81328388+Pupadel@users.noreply.github.com>
2021-09-10 23:23:23 +0300

    Update web2zip.py

--- web2zip.py
+++ web2zip.py
@@ -16,6 +16,7 @@ class Web2zipMod(loader.Module):
 	async def web2zipcmd(self, message):
 		""".web2zip <ссылка на сайт>/nExemple: https://pupadel.ml"""
 		website = utils.get_args_raw(message)
+		website = r'website
 		if not website:
 			await message.edit("Используйте <code>.help Web2zip</code>")
 			return
@@ -26,12 +27,12 @@ class Web2zipMod(loader.Module):
 		await message.edit("<code>Загрузка...</code>")
 		sp.getoutput('apt install wget -y && apt install zip -y')
 		path = sp.getoutput('pwd')
-		output = sp.getoutput(f'wget -r -k -l 30 -p -E -nc {r"website} -P {path}')
-		if f'wget: unable to resolve host address ‘{r"website}’' in output:
+		output = sp.getoutput(f'wget -r -k -l 30 -p -E -nc {website} -P {path}')
+		if f'wget: unable to resolve host address ‘{website}’' in output:
 			await message.edit("<code>Сайт не найден</code>")
 			return
-		sp.getoutput(f'zip -r {r"website}.zip {r"website}/')
-		await message.respond(file=f'{path}/{r"website}.zip')
-		sp.getoutput(f'rm -rf {path}/{r"website}.zip')
-		sp.getoutput(f'rm -rf {path}/{r"website}')
+		sp.getoutput(f'zip -r {website}.zip {website}/')
+		await message.respond(file=f'{path}/{website}.zip')
+		sp.getoutput(f'rm -rf {path}/{website}.zip')
+		sp.getoutput(f'rm -rf {path}/{website}')
 		await message.delete()
