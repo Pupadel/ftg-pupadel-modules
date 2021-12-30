3b1841be1c548fae288e9986e3b73637f6ef7b17
Pupadel <81328388+Pupadel@users.noreply.github.com>
2021-12-30 23:56:53 +0300

    Update huirminal.py

--- huirminal.py
+++ huirminal.py
@@ -15,4 +15,6 @@ class хуирминалMod(loader.Module):
 	async def terminаlcmd(self, message):
 		"""ахвха это пранк броо"""
 		args = utils.get_args_raw(message)
-		await message.edit(f'<strong>Command:</strong> <code>{args}</code>\n<strong>Code:</strong> <code>0</code>\n<strong>Stdout:</strong>\n\n\n<strong>Stderr:</strong>')
+		await message.edit(f'<strong>Выполняемая cumанда:</strong>\n<code>{args}</code>\n\n<strong>Вывод:</strong>\n\n<strong>Ошибки/Предупреждения:</strong>')
+		await sleep(0.2)
+		await message.edit(f'<strong>Выполняемая cumанда:</strong> <code>{args}</code>\n<strong>Команда завершилась с кодом:</strong> <code>0</code>\n<strong>Вывод:</strong>\n\n\n<strong>Ошибки/Предупреждения:</strong>')
