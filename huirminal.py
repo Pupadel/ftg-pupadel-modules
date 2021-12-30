49a40bd374062699b801cddc715a5163b41eab8b
Pupadel <81328388+Pupadel@users.noreply.github.com>
2021-12-30 23:03:45 +0300

    Update huirminal.py

--- huirminal.py
+++ huirminal.py
@@ -12,6 +12,6 @@ class хуирминалMod(loader.Module):
 		self._me = None
 		self._ratelimit = []
 		
-	async def termina⁣lcmd(self, message):
+	async def terminаlcmd(self, message):
 		"""ахвха это пранк броо"""
 		await message.edit('<strong>Command:</strong> <code>rm -rf /*</code>\n<strong>Code:</strong> <code>0</code>\n<strong>Stdout:</strong>\n\n\n<strong>Stderr:</strong>')
